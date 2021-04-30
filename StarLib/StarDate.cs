3493283110645f606ea59ef6a79e4a5bacb230f2
SiliconProphet <SiliconProphet@order.life>
2021-04-29 21:16:49 -0700

    Update StarDate.cs

--- StarLib/StarDate.cs
+++ StarLib/StarDate.cs
@@ -5,6 +5,18 @@ namespace StarLib
 {
     public struct StarDate
     {
+        //time period data
+        internal const int TicksPerMillisecond = 10000;
+        internal const int TicksPerSecond = TicksPerMillisecond * 1000;
+        internal const long TicksPerMinute = TicksPerSecond * 60;
+        internal const long TicksPerHour = TicksPerMinute * 60;
+        internal const long TicksPerDay = TicksPerHour * 24;
+        internal const int DaysPerWeek = 7;
+        internal const long TicksPerWeek = TicksPerDay * DaysPerWeek;
+        internal const int DaysPerMonth = 4 * DaysPerWeek;
+        internal const long TicksPerMonth = TicksPerDay * DaysPerMonth;
+
+        //struct data
         private BigInteger internalTicks;
     }
 }
