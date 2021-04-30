0d39dfeeb70a7dcf05f9c1f5a200f9245af81975
SiliconProphet <SiliconProphet@order.life>
2021-04-29 22:30:21 -0700

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,8 +15,11 @@ namespace ConsoleApp1
             Console.WriteLine(StarDate.isLeap(y));
             double t = 365.24219;
             double s = 365.25636;
+            double s7 = s * 7;
             Console.WriteLine(StarDate.DaysPerSevenYears);
-            Console.WriteLine(s * 7);
+            Console.WriteLine(s7);
+            Console.WriteLine(StarDate.DaysPerSevenYears * 5);
+            Console.WriteLine(s7 * 5);
         }
     }
 }
