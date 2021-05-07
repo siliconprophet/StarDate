0e293177846001453a674c36d98bb7549fb8e73c
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2021-05-06 17:11:46 -0700

    Update StarZone.cs

--- StarLib/StarZone.cs
+++ StarLib/StarZone.cs
@@ -1,6 +1,37 @@
-﻿namespace StarLib
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace StarLib
 {
-    internal class StarZone
+    public class StarZone
     {
+        public static StarZone UTC
+        {
+            get
+            {
+                return new StarZone();
+            }
+        }
+        private ZoneInternal zone = ZoneInternal.UTC;
+        public TimeZoneInfo TimeZoneInfo
+        {
+            get
+            {
+                if (zone == ZoneInternal.UTC)
+                {
+                    return TimeZoneInfo.Utc;
+                }
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    internal enum ZoneInternal
+    {
+        UTC
     }
 }
